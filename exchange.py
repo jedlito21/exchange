11458409dadc2198a9cd9b19e64d712ee21c7a22
tomas <jedlito21@sps-prosek.cz>
2023-10-22 21:46:29 +0200

    dodělat json

--- exchange.py
+++ exchange.py
@@ -1,5 +1,7 @@
 import requests
 import json
+from tkinter import *
+from tkinter import messagebox
 
 #api to my file
 api = requests.get('http://api.exchangeratesapi.io/v1/latest?access_key=187cb0fb71a43cf71f6c1e9acc70d216')
@@ -7,25 +9,59 @@ data = api.json()
 test = data["rates"]
 api = str(api)
 
+def on_enter_press(event):
+    number()
+def number(*args):
+    try:
+        first_currency = options.get()
+        second_currency = options2.get()
+        money = float(cislo.get())
+        to_eur = test[first_currency]
+        to_curr = test[second_currency]
+        math = money / to_eur * to_curr
+        messagebox.showinfo("Result", f"Za {round(money, 2)} {first_currency} budete mít {round(math, 2)} {second_currency}")
+    except ValueError:
+        messagebox.showerror("Chyba", "Zadej platné číslo.")
+
+
 if api == "<Response [200]>":
     with open('venv/currencies.json', 'w') as outfile:
         json.dump(test, outfile)
 
+#listing currencies
+currencies = []
+for index, (key, value) in enumerate(test.items()):
+    currencies.append(key)
 
-#from file to program
-#with open("currencies.json", "r") as file:
+#tkinter
+win = Tk()
+win.geometry("300x200")
+win.title("exchanger")
 
+options = StringVar(win)
+options.set("")
 
-#print(api)
-print("List měn možných k výměně: ")
-print('\n'.join("{}".format(k) for k, v in test.items()))
+options2 = StringVar(win)
+options2.set("")
 
-first_currency = input("Vyber svou první měnu: ")
-second_currency = input("Vyber svou druhou měnu: ")
-money = float(input("Kolik si chcete vyměnit?: "))
+text1 = Label(win,  text='Vybete vaši měnu', width=15)
+text1.pack()
 
-to_eur = test[str(first_currency)]
-to_curr = test[str(second_currency)]
-math = money / to_eur * to_curr
+om1 = OptionMenu(win, options, *currencies)
+om1.pack()
 
-print("Za",money, first_currency, "budete mít", math, second_currency)
+text2 = Label(win,  text='Vybete měnu do které chcete vyměnit peníze', width=40 )
+text2.pack()
+
+om2 = OptionMenu(win, options2, *currencies)
+om2.pack()
+
+cislo = Entry(win, width=20)
+cislo.pack(pady=10)
+
+potvrdit = Button(win, text="Potvrdit", command=number)
+potvrdit.pack()
+
+win.bind('<Return>', on_enter_press)
+
+win.mainloop()
